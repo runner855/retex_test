b3db56b7e5f1b0f0dfa41febe5ce9448e9ade173
Dario Milani <dariomilani855@gmail.com>
2024-09-12 22:59:16 +0200

    working on the search bar

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,18 +1,39 @@
 import React from "react";
 import { CiUser } from "react-icons/ci";
+import { IoMdMenu } from "react-icons/io";
+import { CiSearch } from "react-icons/ci";
 import "../NavBar/NavBar.css";
 
 export const NavBar = () => {
   return (
-    <div>
-      <ul className="navbar_left">
-        <li>Contribuisci</li>
-        <li>Abbonati</li>
-        <li>
-          <CiUser />
-        </li>
-        <li className="accedi">Accedi</li>
-      </ul>
+    <div className="main">
+      <div className="navbar_container_top">
+        <ul className="navbar_left">
+          <li>Contribuisci</li>
+          <li>Abbonati</li>
+          <div className="navbar_right">
+            <li>
+              <CiUser />
+            </li>
+            <li className="accedi">Accedi</li>
+          </div>
+        </ul>
+      </div>
+      <div className="navbar_container_bottom">
+        <ul className="bottom_cont">
+          <div className="hamburger_menu">
+            <li>
+              <IoMdMenu />
+            </li>
+          </div>
+          <li></li>
+          <div className="search">
+            <li>
+              <CiSearch />
+            </li>
+          </div>
+        </ul>
+      </div>
     </div>
   );
 };
